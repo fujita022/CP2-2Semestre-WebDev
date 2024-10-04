0a45c1a25a7bb5c31969f53a77ca82296949d41d
Erick Fujita <erickfujitao@gmail.com>
2024-10-04 01:42:52 -0300

    f

--- cp2-webdev/src/components/Footer/Footer.jsx
+++ cp2-webdev/src/components/Footer/Footer.jsx
@@ -4,15 +4,18 @@ function Footer() {
     <footer className="bg-[#0f1624] text-white py-6">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center mb-4 md:mb-0">
-          <span className="text-lg">Developed by Erick Fujita</span>
-        </div>
+        <span className="text-3xl">
+  Developed by{" "}
+  <span className="text-blue-900 text-5xl font-semibold animate-pulse bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent shadow-lg">
+    Erick Fujita
+  </span>{" "}
+  in 2024
+</span>        </div>
         <div className="flex space-x-4">
-          <a href="mailto:seuemail@example.com" className="flex items-center">
+          <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJTNqLPGDjntHRnvNnRsgFWfVGmgnsLmbfZGlWjmHGSZJmFtgnFMMCvRrKDDpCTMPQZtzNq" className="flex items-center">
             <span className="ml-2">Email</span>
           </a>
-          <a href="tel:+5511999999999" className="flex items-center">
-            <span className="ml-2">Telefone</span>
-          </a>
+
         </div>
       </div>
     </footer>
